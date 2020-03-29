c9258497fe5612189c2aa3891f000b74bda0fa2a
rgo-209 <golhar.rahul@gmail.com>
2020-03-29 04:45:36 -0400

    Cube code working final commented

--- Lab4/Lab4/Shapes.cpp
+++ Lab4/Lab4/Shapes.cpp
@@ -35,158 +35,167 @@ void makeCube( Canvas &C, int subdivisions )
     if( subdivisions < 1 )
         subdivisions = 1;
 
-    // YOUR IMPLEMENTATION HERE
-    
+    // Defining all required points here
     Vertex v_a, v_b;
+    Vertex v_ax_ay_p;
+    Vertex v_ax_ay_n;
+    Vertex v_bx_by_p;
+    Vertex v_bx_by_n;
+    Vertex v_ax_by_p;
+    Vertex v_ax_by_n;
+    Vertex v_bx_ay_p;
+    Vertex v_bx_ay_n;
+    Vertex v_n_ax_by;
+    Vertex v_p_ax_by;
+    Vertex v_n_bx_ay;
+    Vertex v_p_bx_ay;
+    Vertex v_n_bx_by;
+    Vertex v_p_bx_by;
+    Vertex v_n_ax_ay;
+    Vertex v_p_ax_ay;
+    Vertex v_ax_n_by;
+    Vertex v_ax_p_by;
+    Vertex v_bx_n_ay;
+    Vertex v_bx_p_ay;
+    Vertex v_ax_n_ay;
+    Vertex v_ax_p_ay;
+    Vertex v_bx_n_by;
+    Vertex v_bx_p_by;
 
     for (int x = 1; x <= subdivisions; x++)
     {
+        // Compute points on the face
         v_a.x = ( (x - 1) * (1.0 / subdivisions) - unit);
         v_b.x = (    x    * (1.0 / subdivisions) - unit);
 
         for (int y = 1; y <= subdivisions; y++)
         {
+
+            // Compute points on the face
             v_a.y = (  (y - 1) * (1.0 / subdivisions) - unit);
             v_b.y = (     y    * (1.0 / subdivisions) - unit);
 
-            Vertex v_ax_ay_p;
+            // Compute the required points for the face
+
             v_ax_ay_p.x = v_a.x;
             v_ax_ay_p.y = v_a.y;
             v_ax_ay_p.z = unit;
 
-            Vertex v_ax_ay_n;
             v_ax_ay_n.x = v_a.x;
             v_ax_ay_n.y = v_a.y;
             v_ax_ay_n.z = neg_unit;
 
-            Vertex v_bx_by_p;
             v_bx_by_p.x = v_b.x;
             v_bx_by_p.y = v_b.y;
             v_bx_by_p.z = unit;
 
-            Vertex v_bx_by_n;
             v_bx_by_n.x = v_b.x;
             v_bx_by_n.y = v_b.y;
             v_bx_by_n.z = neg_unit;
 
-            Vertex v_ax_by_p;
             v_ax_by_p.x = v_a.x;
             v_ax_by_p.y = v_b.y;
             v_ax_by_p.z = unit;
 
-            Vertex v_ax_by_n;
             v_ax_by_n.x = v_a.x;
             v_ax_by_n.y = v_b.y;
             v_ax_by_n.z = neg_unit;
 
-            Vertex v_bx_ay_p;
             v_bx_ay_p.x = v_b.x;
             v_bx_ay_p.y = v_a.y;
             v_bx_ay_p.z = unit;
 
-            Vertex v_bx_ay_n;
             v_bx_ay_n.x = v_b.x;
             v_bx_ay_n.y = v_a.y;
             v_bx_ay_n.z = neg_unit;
 
-            Vertex v_n_ax_by;
             v_n_ax_by.x = neg_unit;
             v_n_ax_by.y = v_a.x;
             v_n_ax_by.z = v_b.y;
-
-            Vertex v_n_bx_ay;
             v_n_bx_ay.x = neg_unit;
             v_n_bx_ay.y = v_b.x;
             v_n_bx_ay.z = v_a.y;
 
-            Vertex v_n_ax_ay;
             v_n_ax_ay.x = neg_unit;
             v_n_ax_ay.y = v_a.x;
             v_n_ax_ay.z = v_a.y;
-
-            Vertex v_n_bx_by;
+ 
             v_n_bx_by.x = neg_unit;
             v_n_bx_by.y = v_b.x;
             v_n_bx_by.z = v_b.y;
 
-            Vertex v_p_bx_ay;
+
             v_p_bx_ay.x = unit;
             v_p_bx_ay.y = v_b.x;
             v_p_bx_ay.z = v_a.y;
 
-            Vertex v_p_ax_by;
             v_p_ax_by.x = unit;
             v_p_ax_by.y = v_a.x;
             v_p_ax_by.z = v_b.y;
-
-            Vertex v_p_ax_ay;
             v_p_ax_ay.x = unit;
             v_p_ax_ay.y = v_a.x;
             v_p_ax_ay.z = v_a.y;
 
-            Vertex v_p_bx_by;
             v_p_bx_by.x = unit;
             v_p_bx_by.y = v_b.x;
             v_p_bx_by.z = v_b.y;
 
-            Vertex v_ax_n_by;
             v_ax_n_by.x = v_a.x;
             v_ax_n_by.y = neg_unit;
             v_ax_n_by.z = v_b.y;
 
-            Vertex v_bx_n_ay;
             v_bx_n_ay.x = v_b.x;
             v_bx_n_ay.y = neg_unit;
             v_bx_n_ay.z = v_a.y;
 
-            Vertex v_ax_n_ay;
             v_ax_n_ay.x = v_a.x;
             v_ax_n_ay.y = neg_unit;
             v_ax_n_ay.z = v_a.y;
 
-            Vertex v_bx_n_by;
             v_bx_n_by.x = v_b.x;
             v_bx_n_by.y = neg_unit;
             v_bx_n_by.z = v_b.y;
 
-            Vertex v_ax_p_by;
             v_ax_p_by.x = v_a.x;
             v_ax_p_by.y = unit;
             v_ax_p_by.z = v_b.y;
 
-            Vertex v_bx_p_ay;
             v_bx_p_ay.x = v_b.x;
             v_bx_p_ay.y = unit;
             v_bx_p_ay.z = v_a.y;
 
-            Vertex v_ax_p_ay;
             v_ax_p_ay.x = v_a.x;
             v_ax_p_ay.y = unit;
             v_ax_p_ay.z = v_a.y;
 
-            Vertex v_bx_p_by;
             v_bx_p_by.x = v_b.x;
             v_bx_p_by.y = unit;
             v_bx_p_by.z = v_b.y;
 
-
-            C.addTriangle(v_bx_ay_n, v_ax_ay_n, v_ax_by_n);
-            C.addTriangle(v_bx_ay_n, v_ax_ay_n, v_ax_by_n);
-
+           
+            // Face Front
             C.addTriangle(v_ax_ay_p, v_bx_ay_p, v_ax_by_p);
             C.addTriangle(v_ax_by_p, v_bx_ay_p, v_bx_by_p);
             
-            C.addTriangle(v_n_ax_by, v_n_bx_ay, v_n_ax_ay);
-            C.addTriangle(v_n_ax_by, v_n_bx_by, v_n_bx_by);
+            // Face Back
+            C.addTriangle(v_bx_ay_n, v_ax_ay_n, v_ax_by_n);
+            C.addTriangle(v_bx_ay_n, v_ax_ay_n, v_ax_by_n);
 
+            // Face Left
+            C.addTriangle(v_n_ax_by, v_n_bx_ay, v_n_ax_ay);
+            C.addTriangle(v_n_ax_by, v_n_bx_by, v_n_bx_by); 
+
+            // Face Right
             C.addTriangle(v_p_bx_ay, v_p_ax_by, v_p_ax_ay);
             C.addTriangle(v_p_bx_ay, v_p_bx_by, v_p_ax_by);
-            
+
+            // Face Bottom
             C.addTriangle(v_bx_n_ay, v_ax_n_by, v_ax_n_ay);
             C.addTriangle(v_bx_n_ay, v_bx_n_by, v_ax_n_by);
-            
+
+            // Face Top
             C.addTriangle(v_ax_p_by, v_bx_p_ay, v_ax_p_ay);
-            C.addTriangle(v_bx_p_by, v_bx_p_ay, v_ax_p_by);        
+            C.addTriangle(v_bx_p_by, v_bx_p_ay, v_ax_p_by);     
         }
     }
 
